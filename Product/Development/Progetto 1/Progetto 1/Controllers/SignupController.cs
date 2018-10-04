7905fedf332178742c4e55ee4bd90436a3d1f43b
Matan Davidi <37486781+MatanDavidi@users.noreply.github.com>
2018-10-04 18:08:37 +0200

    Added product subfolders and created project
    Added Product/Development and Product/Production.
    Created Visual Studio solution and project for the app.
    Created Home, About, Contact and Signup view files.
    Created model Utente.
    Created controller Signup

---
+++ Product/Development/Progetto 1/Progetto 1/Controllers/SignupController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Progetto_1.Models;
+
+namespace Progetto_1.Controllers
+{
+    public class SignupController : Controller
+    {
+        public IActionResult Index()
+        {
+            Utente u = new Utente();
+            u.Nome = "Big shaq";
+            return View(u);
+        }
+
+        public IActionResult Signup()
+        {
+            return null;
+        }
+    }
+}
