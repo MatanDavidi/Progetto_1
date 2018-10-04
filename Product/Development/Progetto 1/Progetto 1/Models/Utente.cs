e95e4d6eb546a3f4e3a2fa894b97debcd2dbaf5d
Matan Davidi <matan.davidi@samtrevano.ch>
2018-10-04 22:35:17 +0200

    Added length and characters validation
    Added validation for name length and for invalid characters

--- Product/Development/Progetto 1/Progetto 1/Models/Utente.cs
+++ Product/Development/Progetto 1/Progetto 1/Models/Utente.cs
@@ -8,9 +8,9 @@ namespace Progetto_1.Models
 {
     public class Utente
     {
-        private string nome;
-
         [Required(ErrorMessage = "Inserisci un nome valido")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Inserire un nome con un numero di caratteri compreso tra 3 e 50")]
+        [RegularExpression(@"[a-zA-ZäöüÄÖÜçãñ ]", ErrorMessage = "Il nome inserito contiene caratteri non validi")]
         public string Nome { get; set; }
 
     }
