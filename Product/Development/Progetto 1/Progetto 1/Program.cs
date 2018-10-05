136e3e05c42d63041e4847a2677b62d6dd5c80ea
Matan Davidi <37486781+MatanDavidi@users.noreply.github.com>
2018-10-05 14:26:59 +0200

    Visual Studio automatically generated files

--- Product/Development/Progetto 1/Progetto 1/Program.cs
+++ Product/Development/Progetto 1/Progetto 1/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Progetto_1.Models;
 
 namespace WebApplication1
 {
