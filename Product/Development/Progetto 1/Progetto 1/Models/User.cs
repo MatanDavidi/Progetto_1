bf75c73d781f89db79e3332f4d3d81bb8646bb75
Matan Davidi <37486781+MatanDavidi@users.noreply.github.com>
2018-10-12 16:17:57 +0200

    Updated validation

--- Product/Development/Progetto 1/Progetto 1/Models/User.cs
+++ Product/Development/Progetto 1/Progetto 1/Models/User.cs
@@ -10,39 +10,41 @@ namespace Progetto_1.Models
     public class User
     {
         [Required(ErrorMessage = "Inserire un nome")]
-        [StringLength(50, ErrorMessage = "La lunghezza del nome deve essere compresa tra 3 e 50 caratteri", MinimumLength = 3)]
+        [StringLength(50, ErrorMessage = "La lunghezza del nome deve essere al massimo di 50 caratteri")]
         [Display(Name = "Nome")]
+        [RegularExpression("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$", ErrorMessage = "Il nome contiene caratteri non consentiti")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Inserire un cognome")]
-        [StringLength(50, ErrorMessage = "La lunghezza del cognome deve essere compresa tra 3 e 50 caratteri", MinimumLength = 3)]
+        [StringLength(50, ErrorMessage = "La lunghezza del cognome deve essere al massimo di 50 caratteri")]
         [Display(Name = "Cognome")]
+        [RegularExpression("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$", ErrorMessage = "Il cognome contiene caratteri non consentiti")]
         public string LastName { get; set; }
 
-        [DataType(DataType.Date, ErrorMessage = "È stata inserita una data di nascita")]
+        [DataType(DataType.Date, ErrorMessage = "La data di nascita inserita è in un formato non valido")]
         [BindRequired]
         [Required(ErrorMessage = "Inserire una data di nascita")]
         [Display(Name = "Data di nascita")]
         public DateTime Birthday { get; set; }
 
         [Required(ErrorMessage = "Inserire un indirizzo")]
-        [StringLength(50, ErrorMessage = "La lunghezza dell'indirizzo deve essere compresa tra 3 e 50 caratteri", MinimumLength = 3)]
+        [StringLength(50, ErrorMessage = "La lunghezza dell'indirizzo deve essere al massimo di 50 caratteri")]
         [Display(Name = "Indirizzo")]
         public string Address { get; set; }
 
         [Required(ErrorMessage = "Inserire un numero civico")]
-        [StringLength(4, ErrorMessage = "La lunghezza del numero civico deve essere compresa tra 1 e 4 caratteri", MinimumLength = 1)]
-        [RegularExpression("[^a-zA-Z0-9]", ErrorMessage = "Il numero civico inserito contiene caratteri non validi")]
+        [StringLength(3, ErrorMessage = "La lunghezza del numero civico deve essere al massimo di 3 caratteri")]
+        [RegularExpression("(\\d+\\s*([A-Za-z](?![A-Za-z]))?)", ErrorMessage = "Il numero civico inserito è in un formato non valido")]
         [Display(Name = "Numero civico")]
         public string CivicNumber { get; set; }
 
         [Required(ErrorMessage = "Inserire una città")]
-        [StringLength(50, ErrorMessage = "La lunghezza della città deve essere compresa tra 3 e 50 caratteri", MinimumLength = 3)]
+        [StringLength(50, ErrorMessage = "La lunghezza della città deve essere al massimo di 50 caratteri")]
         [Display(Name = "Città")]
         public string City { get; set; }
 
         [BindRequired]
-        [Required(ErrorMessage = "Inserire un NAP")]
+        [Required(ErrorMessage = "Inserire un NAP valido")]
         [StringLength(5, MinimumLength = 4, ErrorMessage = "La lunghezza del NAP deve essere compresa tra 4 e 5 numeri")]
         [Display(Name = "NAP")]
         public int Nap { get; set; }
@@ -50,11 +52,15 @@ namespace Progetto_1.Models
         [Required(ErrorMessage = "Inserire un numero di telefono")]
         [Display(Name = "Numero di telefono")]
         [DataType(DataType.PhoneNumber)]
+        [StringLength(50, ErrorMessage = "La lunghezza del numero di telefono deve essere compresa tra 10 e 50 caratteri")]
+        //[RegularExpression("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$", ErrorMessage = "Il numero di telefono inserito è in un formato non riconosciuto")]
+        [Phone(ErrorMessage = "Il numero di telefono inserito è in un formato non riconosciuto")]
         public string PhoneNumber { get; set; }
 
         [Required(ErrorMessage = "Inserire un indirizzo email")]
-        [EmailAddress(ErrorMessage = "È stata inserita un'email in un formato non riconosciuto")]
         [Display(Name = "Indirizzo e-mail")]
+        [EmailAddress(ErrorMessage = "L'indirizzo email inserito è in un formato non riconosciuto")]
+        [RegularExpression("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$", ErrorMessage = "L'indirizzo email inserito è in un formato non riconosciuto" )]
         public string Email { get; set; }
 
         [Display(Name = "Sesso")]
