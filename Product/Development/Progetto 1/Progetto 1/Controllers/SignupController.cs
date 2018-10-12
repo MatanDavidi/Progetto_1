390996b132ef335b98684fa8bf5b049b416faba2
Matan Davidi <37486781+MatanDavidi@users.noreply.github.com>
2018-10-12 13:19:18 +0200

    Changed button layout and asp action

--- Product/Development/Progetto 1/Progetto 1/Controllers/SignupController.cs
+++ Product/Development/Progetto 1/Progetto 1/Controllers/SignupController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
 using Progetto_1.Models;
 
 namespace Progetto_1.Controllers
@@ -14,9 +16,73 @@ namespace Progetto_1.Controllers
             return View();
         }
 
-        public IActionResult Signup()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(IFormCollection collection)
         {
-            return null;
+            User u = CollectionToUser(collection);
+            return View("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DataCheck(IFormCollection collection)
+        {
+            if (ModelState.IsValid)
+            {
+                User u = CollectionToUser(collection);
+
+                return View("DataCheck", u);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        private User CollectionToUser(IFormCollection collection)
+        {
+            collection.TryGetValue("FirstName", out StringValues fName);
+            collection.TryGetValue("LastName", out StringValues lName);
+            collection.TryGetValue("Birthday", out StringValues bDay);
+            collection.TryGetValue("Address", out StringValues address);
+            collection.TryGetValue("CivicNumber", out StringValues cNumber);
+            collection.TryGetValue("City", out StringValues city);
+            collection.TryGetValue("Nap", out StringValues nap);
+            collection.TryGetValue("PhoneNumber", out StringValues phone);
+            collection.TryGetValue("Email", out StringValues email);
+            collection.TryGetValue("Gender", out StringValues gender);
+            collection.TryGetValue("Hobby", out StringValues hobby);
+            collection.TryGetValue("Job", out StringValues job);
+
+            User u = new User()
+            {
+                FirstName = fName.FirstOrDefault(),
+                LastName = lName.FirstOrDefault(),
+                Address = address.FirstOrDefault(),
+                CivicNumber = cNumber.FirstOrDefault(),
+                City = city.FirstOrDefault(),
+                PhoneNumber = phone.FirstOrDefault(),
+                Email = email.FirstOrDefault(),
+                Gender = gender.FirstOrDefault(),
+                Hobby = hobby.FirstOrDefault(),
+                Job = hobby.FirstOrDefault(),
+            };
+
+            DateTime bd = new DateTime();
+            DateTime.TryParse(bDay.FirstOrDefault(), out bd);
+            u.Birthday = bd;
+
+            int np = new int();
+            int.TryParse(nap.FirstOrDefault(), out np);
+            u.Nap = np;
+            return u;
+        }
+
+        public IActionResult Clear()
+        {
+            ModelState.Clear();
+            return View("Index");
         }
     }
 }
--- Product/Development/Progetto 1/Progetto 1/Models/User.cs
+++ Product/Development/Progetto 1/Progetto 1/Models/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
@@ -10,38 +11,60 @@ namespace Progetto_1.Models
     {
         [Required(ErrorMessage = "Inserire un nome")]
         [StringLength(50, ErrorMessage = "La lunghezza del nome deve essere compresa tra 3 e 50 caratteri", MinimumLength = 3)]
+        [Display(Name = "Nome")]
         public string FirstName { get; set; }
 
+        [Required(ErrorMessage = "Inserire un cognome")]
+        [StringLength(50, ErrorMessage = "La lunghezza del cognome deve essere compresa tra 3 e 50 caratteri", MinimumLength = 3)]
+        [Display(Name = "Cognome")]
         public string LastName { get; set; }
 
+        [DataType(DataType.Date, ErrorMessage = "È stata inserita una data di nascita")]
+        [BindRequired]
+        [Required(ErrorMessage = "Inserire una data di nascita")]
+        [Display(Name = "Data di nascita")]
         public DateTime Birthday { get; set; }
 
+        [Required(ErrorMessage = "Inserire un indirizzo")]
+        [StringLength(50, ErrorMessage = "La lunghezza dell'indirizzo deve essere compresa tra 3 e 50 caratteri", MinimumLength = 3)]
+        [Display(Name = "Indirizzo")]
         public string Address { get; set; }
 
         [Required(ErrorMessage = "Inserire un numero civico")]
         [StringLength(4, ErrorMessage = "La lunghezza del numero civico deve essere compresa tra 1 e 4 caratteri", MinimumLength = 1)]
-        [RegularExpression("[a-zA-Z0-9]", ErrorMessage = "Il numero civico inserito contiene caratteri non validi")]
+        [RegularExpression("[^a-zA-Z0-9]", ErrorMessage = "Il numero civico inserito contiene caratteri non validi")]
+        [Display(Name = "Numero civico")]
         public string CivicNumber { get; set; }
 
         [Required(ErrorMessage = "Inserire una città")]
         [StringLength(50, ErrorMessage = "La lunghezza della città deve essere compresa tra 3 e 50 caratteri", MinimumLength = 3)]
+        [Display(Name = "Città")]
         public string City { get; set; }
 
+        [BindRequired]
         [Required(ErrorMessage = "Inserire un NAP")]
         [StringLength(5, MinimumLength = 4, ErrorMessage = "La lunghezza del NAP deve essere compresa tra 4 e 5 numeri")]
-        [RegularExpression("[0-9]", ErrorMessage = "Il NAP inserito contiene caratteri non validi")]
+        [Display(Name = "NAP")]
         public int Nap { get; set; }
 
         [Required(ErrorMessage = "Inserire un numero di telefono")]
-        [RegularExpression("[0-9 -_]", ErrorMessage = "Il numero di telefono inserito contiene caratteri non consentiti")]
+        [Display(Name = "Numero di telefono")]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
 
+        [Required(ErrorMessage = "Inserire un indirizzo email")]
+        [EmailAddress(ErrorMessage = "È stata inserita un'email in un formato non riconosciuto")]
+        [Display(Name = "Indirizzo e-mail")]
         public string Email { get; set; }
 
+        [Display(Name = "Genere")]
         public string Gender { get; set; }
 
+        [StringLength(500, ErrorMessage = "La lunghezza dell'hobby deve essere al massimo di 500 caratteri")]
         public string Hobby { get; set; }
 
+        [StringLength(500, ErrorMessage = "La lunghezza della professione deve essere al massimo di 500 caratteri")]
+        [Display(Name = "Professione")]
         public string Job { get; set; }
     }
 }
