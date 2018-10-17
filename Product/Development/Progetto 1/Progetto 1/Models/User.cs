e7bb13156cab99825b266709ddbd0bd7951b6cc7
Matan Davidi <37486781+MatanDavidi@users.noreply.github.com>
2018-10-17 21:47:51 +0200

    Added minimum string length to phone number

--- Product/Development/Progetto 1/Progetto 1/Models/User.cs
+++ Product/Development/Progetto 1/Progetto 1/Models/User.cs
@@ -52,7 +52,7 @@ namespace Progetto_1.Models
         [Required(ErrorMessage = "Inserire un numero di telefono")]
         [Display(Name = "Numero di telefono")]
         [DataType(DataType.PhoneNumber)]
-        [StringLength(50, ErrorMessage = "La lunghezza del numero di telefono deve essere compresa tra 10 e 50 caratteri")]
+        [StringLength(50, ErrorMessage = "La lunghezza del numero di telefono deve essere compresa tra 10 e 50 caratteri", MinimumLength = 10)]
         //[RegularExpression("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$", ErrorMessage = "Il numero di telefono inserito è in un formato non riconosciuto")]
         [Phone(ErrorMessage = "Il numero di telefono inserito è in un formato non riconosciuto")]
         public string PhoneNumber { get; set; }
