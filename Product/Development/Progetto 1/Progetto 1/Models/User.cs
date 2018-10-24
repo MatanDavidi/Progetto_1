b532c50710515c88c6affd2ccf0022a070d73671
Matan Davidi <37486781+MatanDavidi@users.noreply.github.com>
2018-10-24 21:23:58 +0200

    Fixed validation according to requirements and generalized text regular expression

--- Product/Development/Progetto 1/Progetto 1/Models/User.cs
+++ Product/Development/Progetto 1/Progetto 1/Models/User.cs
@@ -9,16 +9,21 @@ namespace Progetto_1.Models
 {
     public class User
     {
+        #region =================== costanti ===================
+        public const string TEXTREGEX = "[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð .'-]+$";
+        #endregion
+
+        #region =================== membri & proprietà =========
         [Required(ErrorMessage = "Inserire un nome")]
         [StringLength(50, ErrorMessage = "La lunghezza del nome deve essere al massimo di 50 caratteri")]
         [Display(Name = "Nome")]
-        [RegularExpression("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$", ErrorMessage = "Il nome contiene caratteri non consentiti")]
+        [RegularExpression(TEXTREGEX, ErrorMessage = "Il nome contiene caratteri non consentiti")]
         public string FirstName { get; set; }
 
         [Required(ErrorMessage = "Inserire un cognome")]
         [StringLength(50, ErrorMessage = "La lunghezza del cognome deve essere al massimo di 50 caratteri")]
         [Display(Name = "Cognome")]
-        [RegularExpression("^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$", ErrorMessage = "Il cognome contiene caratteri non consentiti")]
+        [RegularExpression(TEXTREGEX, ErrorMessage = "Il cognome contiene caratteri non consentiti")]
         public string LastName { get; set; }
 
         [DataType(DataType.Date, ErrorMessage = "La data di nascita inserita è in un formato non valido")]
@@ -30,10 +35,11 @@ namespace Progetto_1.Models
         [Required(ErrorMessage = "Inserire un indirizzo")]
         [StringLength(50, ErrorMessage = "La lunghezza dell'indirizzo deve essere al massimo di 50 caratteri")]
         [Display(Name = "Indirizzo")]
+        [RegularExpression(TEXTREGEX, ErrorMessage = "L'indirizzo contiene caratteri non consentiti")]
         public string Address { get; set; }
 
         [Required(ErrorMessage = "Inserire un numero civico")]
-        [StringLength(3, ErrorMessage = "La lunghezza del numero civico deve essere al massimo di 3 caratteri")]
+        [StringLength(4, ErrorMessage = "La lunghezza del numero civico deve essere al massimo di 4 caratteri")]
         [RegularExpression("(\\d+\\s*([A-Za-z](?![A-Za-z]))?)", ErrorMessage = "Il numero civico inserito è in un formato non valido")]
         [Display(Name = "Numero civico")]
         public string CivicNumber { get; set; }
@@ -41,6 +47,7 @@ namespace Progetto_1.Models
         [Required(ErrorMessage = "Inserire una città")]
         [StringLength(50, ErrorMessage = "La lunghezza della città deve essere al massimo di 50 caratteri")]
         [Display(Name = "Città")]
+        [RegularExpression(TEXTREGEX, ErrorMessage = "La città contiene caratteri non consentiti")]
         public string City { get; set; }
 
         [BindRequired]
@@ -53,24 +60,28 @@ namespace Progetto_1.Models
         [Display(Name = "Numero di telefono")]
         [DataType(DataType.PhoneNumber)]
         [StringLength(50, ErrorMessage = "La lunghezza del numero di telefono deve essere compresa tra 10 e 50 caratteri", MinimumLength = 10)]
-        //[RegularExpression("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$", ErrorMessage = "Il numero di telefono inserito è in un formato non riconosciuto")]
+        [RegularExpression("^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\\s\\./0-9]*$", ErrorMessage = "Il numero di telefono inserito è in un formato non riconosciuto")]
         [Phone(ErrorMessage = "Il numero di telefono inserito è in un formato non riconosciuto")]
         public string PhoneNumber { get; set; }
 
         [Required(ErrorMessage = "Inserire un indirizzo email")]
         [Display(Name = "Indirizzo e-mail")]
         [EmailAddress(ErrorMessage = "L'indirizzo email inserito è in un formato non riconosciuto")]
-        [RegularExpression("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$", ErrorMessage = "L'indirizzo email inserito è in un formato non riconosciuto" )]
+        [RegularExpression("^\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$", ErrorMessage = "L'indirizzo email inserito contiene caratteri non consentiti")]
+        [StringLength(50, ErrorMessage = "La lunghezza dell'indirizzo email non può eccedere i 50 caratteri")]
         public string Email { get; set; }
 
         [Display(Name = "Sesso")]
         public string Gender { get; set; }
 
         [StringLength(500, ErrorMessage = "La lunghezza dell'hobby deve essere al massimo di 500 caratteri")]
+        [RegularExpression("^((?![;[\\]{}<>\\\\]).)*$", ErrorMessage = "L'hobby contiene caratteri non consentiti")]
         public string Hobby { get; set; }
 
         [StringLength(500, ErrorMessage = "La lunghezza della professione deve essere al massimo di 500 caratteri")]
         [Display(Name = "Professione")]
+        [RegularExpression("^((?![;[\\]{}<>\\\\]).)*$", ErrorMessage = "La professione contiene caratteri non consentiti")]
         public string Job { get; set; }
+        #endregion
     }
 }
