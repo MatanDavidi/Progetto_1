ced92ca414074d0e644f2cd1ef7f083d15449c90
Matan Davidi <37486781+MatanDavidi@users.noreply.github.com>
2018-11-04 15:23:12 +0100

    Fixed FileNotFoundException
    If you opened the page but there was no file for today the program would throw a "FileNotFoundException", I fixed this issue by wrapping the usage of the method csv.ReadAllLines with a try-catch clause that would catch this exception and return an empty list

--- Product/Development/Progetto 1/Progetto 1/Controllers/ReadCheckController.cs
+++ Product/Development/Progetto 1/Progetto 1/Controllers/ReadCheckController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -13,16 +14,24 @@ namespace Progetto_1.Controllers
         public IActionResult Index()
         {
             CSVHelper csv = new CSVHelper(
-                string.Format("wwwroot/Registrazioni/Registrazioni_{0}_{1}_{2}.csv", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day), 
+                string.Format("wwwroot/Registrazioni/Registrazioni_{0}_{1}_{2}.csv", DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day),
                 ";",
                 new string[] {
                     "data", "nome", "cognome", "data_nascita", "via", "numero_civico",
                     "citta", "nap", "telefono", "email", "sesso", "hobby", "professione" });
             List<User> users = new List<User>();
-            for (int i = 1; i < csv.ReadAllLines().Count; ++i)
+            try
             {
-                User u = new User(csv.ReadAllLines().ElementAt(i));
-                users.Add(u);
+                List<string[]> readFromFile = csv.ReadAllLines();
+                for (int i = 1; i < readFromFile.Count; ++i)
+                {
+                    User u = new User(readFromFile.ElementAt(i));
+                    users.Add(u);
+                }
+            }
+            catch (FileNotFoundException)
+            {
+
             }
 
             return View(users.AsEnumerable());
